d22b6af5c00dfdc0e0078a8fefd0a51d960ac4a3
go95 <go9513@gmail.com>
2019-11-05 10:09:01 +0300

    Update week7.R

--- week7.R
+++ week7.R
@@ -87,6 +87,7 @@ simulation <- function(n, k, h) {
   c(mean(results), var(results))
 }
 
+simulation(1000, 10000, 0.6)
 simulation(1000, 10000, 0.5)
 simulation(1000, 10000, 0.4)
 simulation(1000, 10000, 0.3)
