76efef89f9f59a8bb32d1be8cc88c60d95fcb45d
Калашнов Георгий Юрьевич <gykalashnov@econ.msu.ru>
2019-11-05 10:15:46 +0300

    add 0.8

--- week7.R
+++ week7.R
@@ -87,6 +87,7 @@ simulation <- function(n, k, h) {
   c(mean(results), var(results))
 }
 
+simulation(1000, 10000, 0.8)
 simulation(1000, 10000, 0.7)
 simulation(1000, 10000, 0.6)
 simulation(1000, 10000, 0.5)
